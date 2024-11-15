5fddef10a3f451a35b3204d9b9146df74371d22f
Peter Zhang <me@mby.pw>
2024-11-15 17:15:06 +0000

    deps: install curl first before Docker

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -2,17 +2,12 @@
 
 set -e
 
-DOCKER_INSTALLED=0
+echo "Installing dependencies"
+sudo apt-get install -y build-essential gfortran flex bison libssl-dev libelf-dev device-tree-compiler python3 python3-subprocess-tee python3-dataclasses-json python3-filelock python3-prctl python3-numpy curl gnuplot
+
 if ! [ -x "$(command -v docker)" ]; then
     echo "Installing Docker"
     curl -sSL https://get.docker.com | sh
     sudo usermod -aG docker "$USER"
-    DOCKER_INSTALLED=1
-fi
-
-echo "Installing dependencies"
-sudo apt-get install -y build-essential gfortran flex bison libssl-dev libelf-dev device-tree-compiler python3 python3-subprocess-tee python3-dataclasses-json python3-filelock python3-prctl python3-numpy curl gnuplot
-
-if [ $DOCKER_INSTALLED -eq 1 ]; then
     echo "*** Please log out and log back in to use Docker without sudo ***"
 fi
